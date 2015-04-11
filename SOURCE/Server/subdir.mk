af9f0720dab92850048cf973e923a9c04fd0bb3c
root <root@planetforever.default.tanktarta.uk0.bigv.io>
2015-04-11 18:10:06 +0100

    Server side commit before commiting todays work.

---
+++ SOURCE/Server/subdir.mk
@@ -0,0 +1,233 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../SOURCE/Server/md5.cc 
+
+CPP_SRCS += \
+../SOURCE/Server/AIScript.cpp \
+../SOURCE/Server/Ability2.cpp \
+../SOURCE/Server/AbilityTime.cpp \
+../SOURCE/Server/Account.cpp \
+../SOURCE/Server/ActiveCharacter.cpp \
+../SOURCE/Server/Arena.cpp \
+../SOURCE/Server/Audit.cpp \
+../SOURCE/Server/BroadCast.cpp \
+../SOURCE/Server/ByteBuffer.cpp \
+../SOURCE/Server/Character.cpp \
+../SOURCE/Server/Chat.cpp \
+../SOURCE/Server/ChatChannel.cpp \
+../SOURCE/Server/Combat.cpp \
+../SOURCE/Server/Components.cpp \
+../SOURCE/Server/Config.cpp \
+../SOURCE/Server/ConfigString.cpp \
+../SOURCE/Server/Crafting.cpp \
+../SOURCE/Server/Creature.cpp \
+../SOURCE/Server/CreatureSpawner2.cpp \
+../SOURCE/Server/Debug.cpp \
+../SOURCE/Server/DebugProfiler.cpp \
+../SOURCE/Server/DebugTracer.cpp \
+../SOURCE/Server/DirectoryAccess.cpp \
+../SOURCE/Server/DropTable.cpp \
+../SOURCE/Server/EliteMob.cpp \
+../SOURCE/Server/EssenceShop.cpp \
+../SOURCE/Server/FileReader.cpp \
+../SOURCE/Server/FileReader3.cpp \
+../SOURCE/Server/FriendStatus.cpp \
+../SOURCE/Server/Gamble.cpp \
+../SOURCE/Server/Globals.cpp \
+../SOURCE/Server/HTTPBase.cpp \
+../SOURCE/Server/HTTPDistribute.cpp \
+../SOURCE/Server/IGForum.cpp \
+../SOURCE/Server/Instance.cpp \
+../SOURCE/Server/InstanceScale.cpp \
+../SOURCE/Server/InstanceScript.cpp \
+../SOURCE/Server/Interact.cpp \
+../SOURCE/Server/Item.cpp \
+../SOURCE/Server/ItemSet.cpp \
+../SOURCE/Server/MainWindow.cpp \
+../SOURCE/Server/MainWindow_Event.cpp \
+../SOURCE/Server/NPC.cpp \
+../SOURCE/Server/Packet.cpp \
+../SOURCE/Server/PartyManager.cpp \
+../SOURCE/Server/Preferences.cpp \
+../SOURCE/Server/Quest.cpp \
+../SOURCE/Server/QuestAction.cpp \
+../SOURCE/Server/QuestScript.cpp \
+../SOURCE/Server/RemoteAction.cpp \
+../SOURCE/Server/Report.cpp \
+../SOURCE/Server/Router.cpp \
+../SOURCE/Server/Scenery2.cpp \
+../SOURCE/Server/ScriptCore.cpp \
+../SOURCE/Server/Simulator.cpp \
+../SOURCE/Server/SimulatorBase.cpp \
+../SOURCE/Server/SocketClass3.cpp \
+../SOURCE/Server/Stats.cpp \
+../SOURCE/Server/StringList.cpp \
+../SOURCE/Server/Trade.cpp \
+../SOURCE/Server/URL.cpp \
+../SOURCE/Server/Util.cpp \
+../SOURCE/Server/VirtualItem.cpp \
+../SOURCE/Server/ZoneDef.cpp \
+../SOURCE/Server/ZoneObject.cpp \
+../SOURCE/Server/main.cpp 
+
+CC_DEPS += \
+./SOURCE/Server/md5.d 
+
+OBJS += \
+./SOURCE/Server/AIScript.o \
+./SOURCE/Server/Ability2.o \
+./SOURCE/Server/AbilityTime.o \
+./SOURCE/Server/Account.o \
+./SOURCE/Server/ActiveCharacter.o \
+./SOURCE/Server/Arena.o \
+./SOURCE/Server/Audit.o \
+./SOURCE/Server/BroadCast.o \
+./SOURCE/Server/ByteBuffer.o \
+./SOURCE/Server/Character.o \
+./SOURCE/Server/Chat.o \
+./SOURCE/Server/ChatChannel.o \
+./SOURCE/Server/Combat.o \
+./SOURCE/Server/Components.o \
+./SOURCE/Server/Config.o \
+./SOURCE/Server/ConfigString.o \
+./SOURCE/Server/Crafting.o \
+./SOURCE/Server/Creature.o \
+./SOURCE/Server/CreatureSpawner2.o \
+./SOURCE/Server/Debug.o \
+./SOURCE/Server/DebugProfiler.o \
+./SOURCE/Server/DebugTracer.o \
+./SOURCE/Server/DirectoryAccess.o \
+./SOURCE/Server/DropTable.o \
+./SOURCE/Server/EliteMob.o \
+./SOURCE/Server/EssenceShop.o \
+./SOURCE/Server/FileReader.o \
+./SOURCE/Server/FileReader3.o \
+./SOURCE/Server/FriendStatus.o \
+./SOURCE/Server/Gamble.o \
+./SOURCE/Server/Globals.o \
+./SOURCE/Server/HTTPBase.o \
+./SOURCE/Server/HTTPDistribute.o \
+./SOURCE/Server/IGForum.o \
+./SOURCE/Server/Instance.o \
+./SOURCE/Server/InstanceScale.o \
+./SOURCE/Server/InstanceScript.o \
+./SOURCE/Server/Interact.o \
+./SOURCE/Server/Item.o \
+./SOURCE/Server/ItemSet.o \
+./SOURCE/Server/MainWindow.o \
+./SOURCE/Server/MainWindow_Event.o \
+./SOURCE/Server/NPC.o \
+./SOURCE/Server/Packet.o \
+./SOURCE/Server/PartyManager.o \
+./SOURCE/Server/Preferences.o \
+./SOURCE/Server/Quest.o \
+./SOURCE/Server/QuestAction.o \
+./SOURCE/Server/QuestScript.o \
+./SOURCE/Server/RemoteAction.o \
+./SOURCE/Server/Report.o \
+./SOURCE/Server/Router.o \
+./SOURCE/Server/Scenery2.o \
+./SOURCE/Server/ScriptCore.o \
+./SOURCE/Server/Simulator.o \
+./SOURCE/Server/SimulatorBase.o \
+./SOURCE/Server/SocketClass3.o \
+./SOURCE/Server/Stats.o \
+./SOURCE/Server/StringList.o \
+./SOURCE/Server/Trade.o \
+./SOURCE/Server/URL.o \
+./SOURCE/Server/Util.o \
+./SOURCE/Server/VirtualItem.o \
+./SOURCE/Server/ZoneDef.o \
+./SOURCE/Server/ZoneObject.o \
+./SOURCE/Server/main.o \
+./SOURCE/Server/md5.o 
+
+CPP_DEPS += \
+./SOURCE/Server/AIScript.d \
+./SOURCE/Server/Ability2.d \
+./SOURCE/Server/AbilityTime.d \
+./SOURCE/Server/Account.d \
+./SOURCE/Server/ActiveCharacter.d \
+./SOURCE/Server/Arena.d \
+./SOURCE/Server/Audit.d \
+./SOURCE/Server/BroadCast.d \
+./SOURCE/Server/ByteBuffer.d \
+./SOURCE/Server/Character.d \
+./SOURCE/Server/Chat.d \
+./SOURCE/Server/ChatChannel.d \
+./SOURCE/Server/Combat.d \
+./SOURCE/Server/Components.d \
+./SOURCE/Server/Config.d \
+./SOURCE/Server/ConfigString.d \
+./SOURCE/Server/Crafting.d \
+./SOURCE/Server/Creature.d \
+./SOURCE/Server/CreatureSpawner2.d \
+./SOURCE/Server/Debug.d \
+./SOURCE/Server/DebugProfiler.d \
+./SOURCE/Server/DebugTracer.d \
+./SOURCE/Server/DirectoryAccess.d \
+./SOURCE/Server/DropTable.d \
+./SOURCE/Server/EliteMob.d \
+./SOURCE/Server/EssenceShop.d \
+./SOURCE/Server/FileReader.d \
+./SOURCE/Server/FileReader3.d \
+./SOURCE/Server/FriendStatus.d \
+./SOURCE/Server/Gamble.d \
+./SOURCE/Server/Globals.d \
+./SOURCE/Server/HTTPBase.d \
+./SOURCE/Server/HTTPDistribute.d \
+./SOURCE/Server/IGForum.d \
+./SOURCE/Server/Instance.d \
+./SOURCE/Server/InstanceScale.d \
+./SOURCE/Server/InstanceScript.d \
+./SOURCE/Server/Interact.d \
+./SOURCE/Server/Item.d \
+./SOURCE/Server/ItemSet.d \
+./SOURCE/Server/MainWindow.d \
+./SOURCE/Server/MainWindow_Event.d \
+./SOURCE/Server/NPC.d \
+./SOURCE/Server/Packet.d \
+./SOURCE/Server/PartyManager.d \
+./SOURCE/Server/Preferences.d \
+./SOURCE/Server/Quest.d \
+./SOURCE/Server/QuestAction.d \
+./SOURCE/Server/QuestScript.d \
+./SOURCE/Server/RemoteAction.d \
+./SOURCE/Server/Report.d \
+./SOURCE/Server/Router.d \
+./SOURCE/Server/Scenery2.d \
+./SOURCE/Server/ScriptCore.d \
+./SOURCE/Server/Simulator.d \
+./SOURCE/Server/SimulatorBase.d \
+./SOURCE/Server/SocketClass3.d \
+./SOURCE/Server/Stats.d \
+./SOURCE/Server/StringList.d \
+./SOURCE/Server/Trade.d \
+./SOURCE/Server/URL.d \
+./SOURCE/Server/Util.d \
+./SOURCE/Server/VirtualItem.d \
+./SOURCE/Server/ZoneDef.d \
+./SOURCE/Server/ZoneObject.d \
+./SOURCE/Server/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+SOURCE/Server/%.o: ../SOURCE/Server/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -UWINDOWS_PLATFORM -UUSE_SEH_EXCEPTIONS -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+SOURCE/Server/%.o: ../SOURCE/Server/%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -UWINDOWS_PLATFORM -UUSE_SEH_EXCEPTIONS -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
