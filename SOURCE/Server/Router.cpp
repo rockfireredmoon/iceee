1b282ef9a34602ce22bdcc81ac27c46038a22564
rockfireredmoon <emerald.icemoon@gmail.com>
2015-10-10 15:47:50 +0100

    Win32 fixes.

--- SOURCE/Server/Router.cpp
+++ SOURCE/Server/Router.cpp
@@ -6,8 +6,6 @@
 #include "StringList.h"
 #include <stdlib.h>  //For itoa()
 #include "Util.h"
-#include <sys/socket.h>
-#include <arpa/inet.h>
 
 //This is the main function of the router thread.  A thread must be created for each port
 //since the connecting function will halt until a connection is established.
@@ -197,26 +195,7 @@ void RouterThread :: RunMainLoop(void)
 			int res = sc.Accept();
 			if(res == 0)
 			{
-				socklen_t len;
-				struct sockaddr_storage addr;
-				char ipstr[INET6_ADDRSTRLEN];
-				int port;
-
-				len = sizeof addr;
-				getpeername(sc.ClientSocket, (struct sockaddr*)&addr, &len);
-
-				// deal with both IPv4 and IPv6:
-				if (addr.ss_family == AF_INET) {
-				    struct sockaddr_in *s = (struct sockaddr_in *)&addr;
-				    port = ntohs(s->sin_port);
-				    inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-				} else { // AF_INET6
-				    struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
-				    port = ntohs(s->sin6_port);
-				    inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
-				}
-
-				OnConnect(ipstr);
+				OnConnect(sc.destAddr);
 				//Job is done, get rid of the client.
 				Status = Status_Kick;
 			}
