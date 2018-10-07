e53e440cca2d3ccff24e876cf2d4c88e99b2ac35
rockfireredmoon <rockfire.redmoon@gmail.com>
2018-10-07 19:12:56 +0100

    Was using C++11 where the project is not yet setup for this.

--- SOURCE/Server/System.cpp
+++ SOURCE/Server/System.cpp
@@ -1,11 +1,10 @@
 #include "System.h"
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
 
+
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
 System::SystemService g_System;
 
 namespace System {
@@ -18,16 +17,23 @@ SystemService::~SystemService() {
 }
 
 std::string SystemService::ExecuteCapture(const char* cmd) {
-	std::array<char, 128> buffer;
-	std::string result;
-	std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-	if (!pipe) throw std::runtime_error("popen() failed!");
-	while (!feof(pipe.get())) {
-		if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
-			result += buffer.data();
-	}
-	return result;
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (!feof(pipe)) {
+            if (fgets(buffer, 128, pipe) != NULL)
+                result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
 }
+
 
 int SystemService::Execute(const char* cmd) {
 	return WEXITSTATUS(system(cmd));
